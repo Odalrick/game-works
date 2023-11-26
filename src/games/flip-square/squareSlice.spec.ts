744ba7b5b21d5979cf38d3c05943fefa866dd595
Ulrik Långström <odalrick@gmail.com>
2023-11-26 01:37:30 +0100

    feat(flip-game): add flipping to square model

--- src/games/flip-square/squareSlice.spec.ts
+++ src/games/flip-square/squareSlice.spec.ts
@@ -1,6 +1,13 @@
 import { describe, expect, it } from "@jest/globals"
 
-import reducer, { CellState, parseString, setGrid } from "./squareSlice"
+import reducer, {
+  CellState,
+  setGrid,
+  flip,
+  indexFromCoordinate,
+  coordinateFromIndex,
+  xoParser,
+} from "./squareSlice"
 import * as R from "ramda"
 
 const g = R.pipe(
@@ -37,9 +44,7 @@ describe("game reducer", () => {
     const squareState = reducer(
       undefined,
       setGrid(
-        parseString(
-          "x",
-          "o",
+        xoParser(
           `
               xox
               ooo
@@ -54,5 +59,86 @@ describe("game reducer", () => {
       ooo
       xxx`),
     )
+  })
+
+  const boards: { input: string; expected: string; flip: [number, number] }[] =
+    [
+      {
+        flip: [0, 0],
+        input: `
+            xox
+            ooo
+            xxx`,
+        expected: `
+            xox
+            xoo
+            oox`,
+      },
+      {
+        flip: [1, 1],
+        input: `
+            xox
+            ooo
+            xxx`,
+        expected: `
+            xxx
+            xxx
+            xox`,
+      },
+      {
+        flip: [2, 2],
+        input: `
+            ooo
+            ooo
+            ooo`,
+        expected: `
+            oxx
+            oox
+            ooo`,
+      },
+    ]
+
+  boards.forEach((board) => {
+    it("should flip", () => {
+      const squareState = reducer(
+        reducer(undefined, setGrid(xoParser(board.input))),
+        flip(board.flip),
+      )
+
+      expect(squareState.toString()).toEqual(g(board.expected))
+    })
+  })
+})
+
+describe("grid functions", () => {
+  it("should generate the correct order of indices", () => {
+    // The y coordinate goes _up_, but is reversed in the array.
+    expect(
+      R.map(indexFromCoordinate, [
+        [0, 0],
+        [1, 0],
+        [2, 0],
+        [0, 1],
+        [1, 1],
+        [2, 1],
+        [0, 2],
+        [1, 2],
+        [2, 2],
+      ]),
+    ).toEqual([6, 7, 8, 3, 4, 5, 0, 1, 2])
+  })
+
+  it("should generate the correct order of coordinates", () => {
+    expect(R.map(coordinateFromIndex, [6, 7, 8, 3, 4, 5, 0, 1, 2])).toEqual([
+      [0, 0],
+      [1, 0],
+      [2, 0],
+      [0, 1],
+      [1, 1],
+      [2, 1],
+      [0, 2],
+      [1, 2],
+      [2, 2],
+    ])
   })
 })
--- src/games/flip-square/squareSlice.ts
+++ src/games/flip-square/squareSlice.ts
@@ -11,9 +11,21 @@ type Grid = CellState[]
 type SquareState = {
   grid: Grid
 }
+type Coordinate = [number, number]
+
+export function indexFromCoordinate(c: Coordinate): number {
+  const [x, y] = c
+  return x + y * -3 + 6
+}
+
+export function coordinateFromIndex(i: number): Coordinate {
+  const x = i % 3
+  const y = Math.abs(Math.floor((i - 6) / 3) * -1)
+  return [x, y]
+}
 
 function getCell(grid: Grid, x: number, y: number): CellState {
-  return grid[x + y * 3]
+  return grid[indexFromCoordinate([x, y])]
 }
 
 function asString(grid: Grid): string {
@@ -35,6 +47,42 @@ export const xoParser = (str: string): Grid => {
   return parseString("x", "o", str)
 }
 
+export function flips(flips: Coordinate[], grid: Grid): Grid {
+  const indices = R.map(indexFromCoordinate, flips)
+  const mapIndexed = R.addIndex(R.map) as (
+    fn: (cell: CellState, idx: number) => CellState,
+    list: CellState[],
+  ) => CellState[]
+
+  return mapIndexed((cell: CellState, i: number) => {
+    if (R.includes(i, indices)) {
+      return cell === CellState.ON ? CellState.OFF : CellState.ON
+    }
+    return cell
+  }, grid)
+}
+
+export function neighbors(c: Coordinate): Coordinate[] {
+  const [x, y] = c
+  return [
+    [x, y],
+    [x, y - 1],
+    [x, y + 1],
+    [x - 1, y],
+    [x + 1, y],
+  ]
+}
+
+const validCoordinate = (c: Coordinate): boolean => {
+  const [x, y] = c
+  return x >= 0 && x < 3 && y >= 0 && y < 3
+}
+
+const filterValidCoordinates = R.filter(validCoordinate)
+
+export function flipNeighbors(c: Coordinate, grid: Grid): Grid {
+  return flips(filterValidCoordinates(neighbors(c)), grid)
+}
 class Square {
   state: SquareState
   constructor(public grid: Grid) {
@@ -69,8 +117,11 @@ export const squareSlice = createSlice({
     setGrid: (_state: Square, action: PayloadAction<Grid>) => {
       return new Square(action.payload)
     },
+    flip: (state: Square, action: PayloadAction<[number, number]>) => {
+      return new Square(flipNeighbors(action.payload, state.state.grid))
+    },
   },
 })
 
-export const { setGrid } = squareSlice.actions
+export const { setGrid, flip } = squareSlice.actions
 export default squareSlice.reducer
